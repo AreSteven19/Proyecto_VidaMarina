c54b4e9c12348709802bde819a80269a809038c4
AreSteven19 <arestevenacevedopereira@gmail.com>
2024-06-27 19:33:08 -0600

    ajute json

--- Navegacion/Register.js
+++ Navegacion/Register.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import {
@@ -8,43 +7,36 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  SafeAreaView ,
+  SafeAreaView,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 
- import app from "../ConexionDB";
- import { addDoc, collection, getFirestore } from "firebase/firestore";
-
+import app from "../ConexionDB";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 export default function Registro() {
-
   const navigation = useNavigation();
- const db = getFirestore(app);
+  const db = getFirestore(app);
 
-  //se puede hacer si esto en el UseState se inicia en 0
   const DatosUsuario = {
-    Nombre: '',
-    Correo: '',
-    Contra: '',
-
+    Nombre: "",
+    Correo: "",
+    Contra: "",
   };
 
   const [EstadoUsuario, setEstado] = useState(DatosUsuario);
 
   const HandleChangeText = (value, name) => {
-
     setEstado({ ...EstadoUsuario, [name]: value });
   };
 
-
   const EnviarDatosFirebase = async () => {
     try {
-      alert("Registro del producto exitoso")
+      alert("Registro del producto exitoso");
       const docRef = await addDoc(collection(db, "Usuario"), EstadoUsuario);
       navigation.navigate("Login");
-
     } catch (e) {
       console.error("No se pudo agregar los datos a la base de datos", e);
     }
@@ -52,90 +44,80 @@ export default function Registro() {
 
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Image style={styles.ImageLog} source={require("../images/logo.jpg")} />
+          <Text style={styles.textologo}>Registro</Text>
+        </View>
 
-    <View style={styles.nav}>
+        <View style={styles.inputs}>
+          <View style={styles.inputytext}>
+            <Text style={styles.label}>Nombre</Text>
+            <TextInput
+              placeholder="Nombre Usuario"
+              style={styles.txtInput}
+              onChangeText={(value) => HandleChangeText(value, "Nombre")}
+              value={EstadoUsuario.Nombre}
+            />
+          </View>
 
-    <Image style={styles.ImageLog} source={require("../images/logo.jpg")} />
-    <Text style={styles.textologo}>Registro</Text>
+          <View style={styles.inputytext}>
+            <Text style={styles.label}>Correo Electrónico</Text>
+            <TextInput
+              placeholder="Correo Electrónico"
+              style={styles.txtInput}
+              onChangeText={(value) => HandleChangeText(value, "Correo")}
+              value={EstadoUsuario.Correo}
+            />
+          </View>
 
+          <View style={styles.inputytext}>
+            <Text style={styles.label}>Contraseña</Text>
+            <TextInput
+              placeholder="Contraseña"
+              secureTextEntry={true}
+              style={styles.txtInput}
+              onChangeText={(value) => HandleChangeText(value, "Contra")}
+              value={EstadoUsuario.Contra}
+            />
+          </View>
 
-    </View>
+          <TouchableOpacity onPress={EnviarDatosFirebase}>
+            <LinearGradient
+              colors={["#5FBCFF", "#5FBCFF"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+              style={styles.btnLogin}
+            >
+              <Text style={styles.txtLogin}>Crear Cuenta</Text>
+            </LinearGradient>
+          </TouchableOpacity>
 
-    <View style={styles.inputs}>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <LinearGradient
+              colors={["#E04F46", "#E04F46"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+              style={styles.btnLogin2}
+            >
+              <Text style={styles.txtLogin}>Cancelar</Text>
+            </LinearGradient>
+          </TouchableOpacity>
 
-    
-
-    <View style={styles.inputytext}>
-    <Text style={styles.a}>Nombre</Text>
-      <TextInput
-        placeholder="Nombre Usuario"
-        style={styles.txtInput}
-        onChangeText={(value) => HandleChangeText(value, 'Nombre')}
-        value={EstadoUsuario.Nombre}
-      ></TextInput>
+          <StatusBar style="auto" />
+        </View>
       </View>
-
-      <TextInput
-        placeholder="Corro Electronico"
-        style={styles.txtInput}
-        onChangeText={(value) => HandleChangeText(value, 'Correo')}
-        value={EstadoUsuario.Correo}
-      ></TextInput>
-
-      <TextInput
-        placeholder="Contraseña"
-        secureTextEntry={true}
-        style={styles.txtInput}
-        onChangeText={(value) => HandleChangeText(value, 'Contra')}
-        value={EstadoUsuario.Contra}
-      ></TextInput>
-
-
-
-      <TouchableOpacity onPress={EnviarDatosFirebase}>
-      <LinearGradient
-            colors={["#5FBCFF", "#5FBCFF"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            style={styles.btnLogin}
-          >
-          <Text style={styles.txtLogin}>Crear Cuenta</Text>
-        </LinearGradient>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-      <LinearGradient
-            colors={["#E04F46", "#E04F46"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            style={styles.btnLogin2}
-          >
-          <Text style={styles.txtLogin}>Cancelar</Text>
-        </LinearGradient>
-      </TouchableOpacity>
-
-
-
-      <StatusBar style="auto" />
-
-      </View>
-    </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-  
     height: "100%",
   },
   ImageLog: {
     height: 50,
     width: 50,
-    
-    
-
   },
   txtTitulo: {
     fontSize: 40,
@@ -157,19 +139,19 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 30,
     paddingLeft: 30,
-    marginTop: 30,
+    marginTop: 10,
     marginLeft: 35,
     borderColor: "gray",
     color: "#000000",
     backgroundColor: "#D9D9D9",
     fontSize: 17,
   },
-  txtPass: {
-    textAlign: "right",
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: 50,
     marginTop: 20,
-    marginRight: 50,
-    color: "#00C1BB",
-    fontSize: 18,
+    color: "#34434D",
   },
   btnLogin: {
     borderRadius: 10,
@@ -185,22 +167,6 @@ const styles = StyleSheet.create({
     color: "#fff",
     textAlign: "center",
   },
-  txtCuenta: {
-    textAlign: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    color: "#00C1BB",
-    fontSize: 15,
-    alignItems: "center",
-  },
-  txtRegistrarse: {
-    textAlign: "center",
-    color: "#00C1BB",
-    fontSize: 15,
-    alignItems: "center",
-    fontWeight: "bold",
-  },
-
   nav: {
     display: "flex",
     justifyContent: "flex-start",
@@ -208,37 +174,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     paddingLeft: 20,
-   
-
     backgroundColor: "#02A3C7",
     height: 100,
-
   },
-
   textologo: {
     color: "#FFFFFF",
     fontSize: 30,
     paddingLeft: 20,
-    fontWeight: "bold"
-
+    fontWeight: "bold",
   },
-
   inputs: {
-    marginTop: 40
-
+    marginTop: 40,
   },
-
-  btnLogin2:{
+  inputytext: {
+    width: "100%",
+  },
+  btnLogin2: {
     borderRadius: 10,
     width: 190,
     height: 40,
     marginTop: 40,
     alignSelf: "center",
     paddingTop: 10,
-
   },
-
-
-
-
 });
